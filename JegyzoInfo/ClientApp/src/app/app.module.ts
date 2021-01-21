ba2b27589ae0e8c5042418a0e3548bb41159adee
Varga Kálmán <kalamane007@gmail.com>
2021-01-21 09:41:50 +0100

    topics start

--- JegyzoInfo/ClientApp/src/app/app.module.ts
+++ JegyzoInfo/ClientApp/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NavigationComponent } from './components/_core/navigation/navigation.component';
 import { FooterComponent } from './components/_core/footer/footer.component';
+import { TopicComponent } from './components/topic/topic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    TopicComponent
   ],
   imports: [
     BrowserModule,
