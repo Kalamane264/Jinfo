da3ff79023bbd247cc23aa2701e7fd9cce693135
Varga Kálmán <kalamane007@gmail.com>
2021-02-02 22:49:40 +0100

    topicOpener

--- JegyzoInfo/ClientApp/src/app/components/chapter/chapter.component.ts
+++ JegyzoInfo/ClientApp/src/app/components/chapter/chapter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chapter',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChapterComponent implements OnInit {
 
+  @Input() opened = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  open(){
+    this.opened = !this.opened;
+  }
 }
