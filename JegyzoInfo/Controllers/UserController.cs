3108a4707dea5cdc689eb5041bde86e4a268e4d2
Varga Kálmán <kalamane007@gmail.com>
2021-02-09 16:40:39 +0100

    ...

---
+++ JegyzoInfo/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JegyzoInfo.Controllers
+{
+    public class UserController : Controller
+    {
+        public bool Login()
+        {
+            Pwi2.WSSoapClient pwi2 = new Pwi2.WSSoapClient();
+
+            return true;
+        }
+    }
+}
